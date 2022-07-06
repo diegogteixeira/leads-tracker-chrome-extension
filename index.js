7e071caf6c8025d122a8ee9abbf14640b2ec7700
Diego G. Teixeira <diegogteixeira@gmail.com>
2022-07-06 15:35:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+let myLeads = [];
+
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const tabBtn = document.getElementById("tab-btn");
+const deleteBtn = document.getElementById("delete-btn");
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+
+function render(leads) {
+  let listItems = "";
+
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+      <li>
+        <a href="${leads[i]}" target="_blank">${leads[i]}</a>
+      </li>
+    `;
+  }
+
+  ulEl.innerHTML = listItems;
+}
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
+
+tabBtn.addEventListener("click", function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+
+deleteBtn.addEventListener("dblclick", function() {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+
+
+// for (let i = 0; i < myLeads.length; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = myLeads[i];
+//   ulEl.append(li);
+// }
+
+// function render(leads) {
+//   ulEl.textContent = "";
+
+//   for (let i = 0; i < leads.length; i++) {
+//     const li = document.createElement("li");
+//     const a = document.createElement("a");
+//     a.textContent = leads[i];
+//     a.href = `https://${leads[i]}`;
+//     a.target = "_blank";
+//     li.appendChild(a);
+//     ulEl.appendChild(li);
+//   }
+// }
